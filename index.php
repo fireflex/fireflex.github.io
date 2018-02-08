e8c241bd112be0cef67254e668c4d92a70ebeada
Jaime Bravo <jaime.senavalle@gmail.com>
2018-02-08 14:30:15 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,87 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>JB | Web Developer</title>
+	<meta name="Web developer, webapps." content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="apple-touch-icon" href="apple-touch-icon.png">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/font-awesome.css">
+	<link rel="stylesheet" href="css/main.css">
+        <!--[if lt IE 9]>
+            <script src="js/vendor/html5-3.6-respond-1.4.2.min.js"></script>
+         <![endif]-->
+      </head>
+      <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+         <![endif]-->
+         <div class="container">
+         	<div class="row">
+         		<div class="col-2">
+         			<nav class="navbar navbar-light">
+         				<button class="navbar-toggler hidden-sm-up" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><i class="fa fa-list"></i></button>
+         				<div class="collapse navbar-toggleable-xs" id="navbarResponsive">
+         					<ul class="nav navbar-nav">
+         						<li class="nav-item active">
+         							<a class="nav-link" href="#">Portfolio<span class="sr-only">(current)</span></a>
+         						</li>
+         						<li class="nav-item">
+         							<a class="nav-link" href="#">Blog</a>
+         						</li>
+         						<li class="nav-item">
+         							<a class="nav-link" href="#">Contact</a>
+         						</li>
+         					</ul>
+         				</div>
+         			</nav>
+         		</div>
+
+         	</div>
+         	<div class="row">
+         		<div class="col-lg-12 title">
+         			<h3>I'm web developer. Build Amazing web apps. :)</h3>
+         		</div>
+         	</div>
+         	<div class="row">
+         		<div class="col-12">
+         			<ul class="nav">
+         				<li><a href="https://github.com/fireflex"><i class="fa fa-github"></i> Github</a></li>
+         				<li><a href="https://github.com/fireflex" <i class="fa fa-twitter"></i> Twitter</a></li>
+         				<li><a href="https://github.com/fireflex"><i class="fa fa-linkedin"></i> LinkedIn</a></li>
+         			</ul>
+         		</div>
+         	</div>
+         </div>
+         <footer>
+         	<div class="container">
+         		<div class="row">
+         			<div class="col-12 text-center">
+         				<p>Jaime Bravo <?= date('Y')?></p>
+         			</div>
+         		</div>
+         	</div>
+         </footer>
+      </div> <!-- /container -->       
+      <script src="js/jquery-3.2.1.min.js"></script>
+      <script src="js/popper.min.js"></script>
+      <script src="js/bootstrap.min.js"></script>
+      <script src="js/plugins.js"></script>
+      <script src="js/main.js"></script>
+
+      <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+      <script>
+      	(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+      		function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+      	e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+      	e.src='//www.google-analytics.com/analytics.js';
+      	r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+      	ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+      </script>
+   </body>
+   </html>
